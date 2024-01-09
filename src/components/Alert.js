a9bb9619aae35c6a75ba7b644fc5b63c301c2827
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-09 11:36:25 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import './Alert.css'
 export default function Alert(props) {
   const Cap = (word) => {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
   return (
-    <div style={{ fontSize: '1.7rem' }} className='al mb-2'>
-      {props.alert && <div style={{ padding: '1.4rem' }} className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <div style={{ fontSize: '1.4rem' }} className='al mb-2'>
+      {props.alert && <div style={{ padding: '1.3rem' }} className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{Cap(props.alert.type)}</strong>:{props.alert.msg}
       </div>}
     </div>
