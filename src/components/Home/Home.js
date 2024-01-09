0cd91856267cab6e75c5c06171fd97667b552e79
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-09 09:14:35 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,15 +24,15 @@ function Home(props) {
                 <form className='mb-2 mt-4' style={{ fontSize: "1.8rem" }}>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control " style={{ height: "4rem",fontSize:'1.4rem' }} id='title' name='title' minLength={3} value={note.title} required onChange={onChange} aria-describedby="emailHelp" />
+                        <input type="text" className="form-control " style={{ height: "4rem",fontSize:'1.28rem' }} id='title' name='title' minLength={3} value={note.title} required onChange={onChange} aria-describedby="emailHelp" />
                     </div>
                     <div className="my-4">
                         <label htmlFor="description" className="form-label" >Description</label>
-                        <input type="text" className="form-control" id='description' style={{ height: "13rem",fontSize:'1.4rem' }} minLength={5} value={note.description} required onChange={onChange} name='description' />
+                        <input type="text" className="form-control" id='description' style={{ height: "13rem",fontSize:'1.28rem' }} minLength={5} value={note.description} required onChange={onChange} name='description' />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label" >Tag</label>
-                        <input type="text" className="form-control" id='Tag' style={{ height: "4rem",fontSize:'1.4rem' }} value={note.tag} required onChange={onChange} name='tag' />
+                        <input type="text" className="form-control" id='Tag' style={{ height: "4rem",fontSize:'1.28rem' }} value={note.tag} required onChange={onChange} name='tag' />
                     </div>
                     <button type="submit" disabled={note.description.length < 5 || note.description.length < 5} className="button-84 my-3" onClick={handleClick}>Create Note</button>
 
