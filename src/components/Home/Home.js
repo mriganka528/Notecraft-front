575cc42bcac36afb1ecef17e5e6c4f4ea1e8963b
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-09 09:07:43 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,19 +20,19 @@ function Home(props) {
     return (
         <>
             <div className={`cont ${props.theme}`} style={{ marginTop: "5rem", background: 'transparent' }}>
-                <h1>Add a note</h1>
-                <form className='mb-2 mt-4' style={{ fontSize: "2rem" }}>
+                <h2>Add a note</h2>
+                <form className='mb-2 mt-4' style={{ fontSize: "1.8rem" }}>
                     <div className="mb-3">
                         <label htmlFor="title" className="form-label">Title</label>
-                        <input type="text" className="form-control " style={{ height: "4rem",fontSize:'1.6rem' }} id='title' name='title' minLength={3} value={note.title} required onChange={onChange} aria-describedby="emailHelp" />
+                        <input type="text" className="form-control " style={{ height: "4rem",fontSize:'1.4rem' }} id='title' name='title' minLength={3} value={note.title} required onChange={onChange} aria-describedby="emailHelp" />
                     </div>
                     <div className="my-4">
                         <label htmlFor="description" className="form-label" >Description</label>
-                        <input type="text" className="form-control" id='description' style={{ height: "13rem",fontSize:'1.6rem' }} minLength={5} value={note.description} required onChange={onChange} name='description' />
+                        <input type="text" className="form-control" id='description' style={{ height: "13rem",fontSize:'1.4rem' }} minLength={5} value={note.description} required onChange={onChange} name='description' />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="tag" className="form-label" >Tag</label>
-                        <input type="text" className="form-control" id='Tag' style={{ height: "4rem",fontSize:'1.6rem' }} value={note.tag} required onChange={onChange} name='tag' />
+                        <input type="text" className="form-control" id='Tag' style={{ height: "4rem",fontSize:'1.4rem' }} value={note.tag} required onChange={onChange} name='tag' />
                     </div>
                     <button type="submit" disabled={note.description.length < 5 || note.description.length < 5} className="button-84 my-3" onClick={handleClick}>Create Note</button>
 
