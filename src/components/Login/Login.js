9470d8938e7aef88ce7c43a8740d4fd17f09e70d
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-09 18:15:44 +0530

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import './Login.css'
+import './Login.css';
 
 function Login(props) {
     const [credentials, setCredentials] = useState({ email: "", password: "" });
@@ -33,16 +33,16 @@ function Login(props) {
         setCredentials({ ...credentials, [e.target.name]: e.target.value });
     }
     return (
-        <form className={`login_form ${props.theme}`} onSubmit={handleSubmit} style={{ fontSize: "2rem" }}>
+        <form className={` login_form ${props.theme}`} onSubmit={handleSubmit}>
             <div className=" mb-4">
                 <h2>Sign in</h2>
             </div>
-            <div className=" pass_div mb-3" style={{ fontSize: '1.8rem' }} >
+            <div className=" pass_div mb-3" style={{ fontSize: '1.65rem' }} >
                 <label htmlFor="email" className="form-label">Email address</label>
                 <input type="email" className="pass_inp form-control" value={credentials.email} name='email' onChange={onChange} id="email" aria-describedby="emailHelp" placeholder="Enter email" style={{ fontSize: '1.3rem' }} />
                 <small id="emailHelp" style={{ fontSize: '1.1rem' }} className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div className="mb-3" style={{ fontSize: '1.8rem' }}>
+            <div className="mb-3" style={{ fontSize: '1.65rem' }}>
                 <label htmlFor="password" className="form-label">Password</label>
                 <input type="password" className="form-control" style={{ fontSize: '1.3rem' }} id="password" name='password' value={credentials.password} onChange={onChange} placeholder="Password" />
             </div>
