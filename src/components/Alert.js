83f2c4bd8004d9190d129dfcfd3f74f414d7da88
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-09 11:49:17 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -6,7 +6,7 @@ export default function Alert(props) {
   }
   return (
     <div style={{ fontSize: '1.4rem' }} className='al mb-2'>
-      {props.alert && <div style={{ padding: '1.3rem' }} className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+      {props.alert && <div style={{ padding: '1.2rem' }} className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{Cap(props.alert.type)}</strong>:{props.alert.msg}
       </div>}
     </div>
