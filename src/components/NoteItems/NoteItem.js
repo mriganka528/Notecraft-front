23658b5eb4de323ef8ab2888783dfa3169574d77
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-01-09 09:36:38 +0530

    Update NoteItem.js

--- src/components/NoteItems/NoteItem.js
+++ src/components/NoteItems/NoteItem.js
@@ -10,8 +10,8 @@ const NoteItem = (props) => {
         <>
             <div className={` ${props.theme} card mr-3 my-3`}  style={{ width: "20rem" }}>
                 <div className="card-body  ">
-                    <h3 className="card-title">{props.note.title}</h3>
-                    <p style={{fontSize:'1.3rem'}} className="card-text">{props.note.description}  </p>
+                    <h4 className="card-title">{props.note.title}</h4>
+                    <p style={{fontSize:'1.15rem'}} className="card-text">{props.note.description}  </p>
                     <div>
                         <i className="fa-solid fa-xl fa-trash mx-2" onClick={() => { deleteNote(note._id); showAlert(" Deleted successfully", "success"); }} ></i>
                         <i className="fa-solid  fa-xl fa-pen-to-square mx-2" onClick={() => { updateNote(note) }} ></i>
